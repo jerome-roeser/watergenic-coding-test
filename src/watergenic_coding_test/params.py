7bb6cc438c656b49851dae19c6a652e456b8f422
jerome roeser <jerome.roeser@gmail.com>
2025-07-12 15:39:50 +0200

    feat(ML-script): create configuration file for easily renaming train/test files if needed. create utils.py for loading config and params.py for defining paths.

---
+++ src/watergenic_coding_test/params.py
@@ -0,0 +1,10 @@
+from pathlib import Path
+from src.utils.utils import config
+
+
+repo_path = Path(__file__).parent.parent.parent
+
+LOCAL_DATA_PATH = Path(repo_path).joinpath(config['folders']['data'])
+LOCAL_MODELS_PATH = Path(repo_path).joinpath(config['folders']['models'])
+
+print(repo_path)
